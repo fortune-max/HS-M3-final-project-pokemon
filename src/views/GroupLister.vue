edbbe985d5b6a72259dbd2a1c68a7516bcce6316
lordfme <fortmax120@gmail.com>
2022-11-24 12:43:35 +0100

    setup Suspense, added minimal styling GroupLister

--- src/views/GroupLister.vue
+++ src/views/GroupLister.vue
@@ -3,6 +3,7 @@
     const props = defineProps(["mainCategory"]);
     const mainCategory = toRef(props, "mainCategory");
     const list = ref([]);
+    const triggerSuspense = ref(0);
 
     async function getList(category){
         const res = await fetch(`https://pokeapi.co/api/v2/${category}`);
@@ -12,6 +13,7 @@
     }
 
     watch(mainCategory, async () => {
+        triggerSuspense.value++;
         list.value = await getList(mainCategory.value);
     });
 
@@ -19,7 +21,25 @@
 </script>
 
 <template>
-    <div v-for="item in list">
-        <a href="item.url">{{item.name}}</a>
-    </div>
+    <Suspense :key="triggerSuspense">
+        <div class="links-container">
+            <div v-for="item in list">
+                <a :href="item.url">{{item.name}}</a>
+            </div>
+        </div>
+        <template #fallback>
+            Loading {{mainCategory}} categories...
+        </template>
+    </Suspense>
 </template>
+
+<style scoped>
+.links-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    margin: 20px;
+}
+</style>
