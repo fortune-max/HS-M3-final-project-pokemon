b9f369e3d782985f850f2388d26488f55dfa3630
lordfme <fortmax120@gmail.com>
2022-11-24 18:45:34 +0100

    upload LoadingCard Suspense placeholder component

---
+++ src/components/LoadingCard.vue
@@ -0,0 +1,37 @@
+<script setup>
+    defineProps(["width", "height"]);
+</script>
+
+<template>
+    <div class="fast-gradient" :style="{height, width}" style="background-color: aqua;">
+
+    </div>
+</template>
+
+<style scoped>
+    .fast-gradient {
+        background: linear-gradient(
+            to right,
+            rgba(255, 255, 255, 0),
+            rgba(255, 255, 255, 1)
+        );
+        background-size: 200% 200%;
+        animation: gradient 2s ease-in-out infinite;
+        z-index: 10;
+
+        border: solid 2px black;
+        border-radius: 5px;
+    }
+
+    @keyframes gradient {
+        0% {
+            background-position: 0% 50%;
+        }
+        50% {
+            background-position: 100% 50%;
+        }
+        100% {
+            background-position: 0% 50%;
+        }
+    }
+</style>
