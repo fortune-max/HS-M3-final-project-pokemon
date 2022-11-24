97353d75dc831607322784fd0942c1f17236a2ee
lordfme <fortmax120@gmail.com>
2022-11-24 17:19:16 +0100

    upload Header and Footer components

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="footer-wrapper">
+        Copyright ©2022
+    </div>
+</template>
+
+<style scoped>
+    .footer-wrapper {
+        background-color: cornflowerblue;
+        height: 100px;
+        text-align: right;
+        padding: 10px;
+        color: white;
+    }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="header-wrapper">
+        <img src="../assets/pokemon_logo.png"/>
+        <nav>
+            <router-link to="/group/type">Type</router-link>
+            <router-link to="/group/pokemon-color">Color</router-link>
+            <router-link to="/group/pokemon-habitat">Habitat</router-link>
+            <router-link to="/group/pokemon-shape">Shape</router-link>
+            <router-link to="/legacy-lookup">Legacy Lookup</router-link>
+        </nav>
+    </div>
+</template>
+
+<style scoped>
+    .header-wrapper {
+        background-color: cornflowerblue;
+    }
+
+    img {
+        width: 30%;
+        padding-left: 10px;
+    }
+
+    nav {
+        display: flex;
+        justify-content: flex-end;
+        gap: 5px;
+        padding-right: 10px;
+        padding-bottom: 10px;
+    }
+</style>
